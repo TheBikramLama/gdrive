eae6b9f549b6e0daaa3d9b658c84841700a318b6
Bikram Lama <TheBikramLama1123@gmail.com>
2020-11-01 21:25:43 +0545

    Updated Credentials

--- handlers_drive.go
+++ handlers_drive.go
@@ -14,8 +14,8 @@ import (
 	"github.com/prasmussen/gdrive/drive"
 )
 
-const ClientId = "367116221053-7n0vf5akeru7on6o2fjinrecpdoe99eg.apps.googleusercontent.com"
-const ClientSecret = "1qsNodXNaWq1mQuBjUjmvhoO"
+const ClientId = "72246848621-66j08il85g6uhpud9j1q322ctoh56cul.apps.googleusercontent.com"
+const ClientSecret = "Oh-ETNAHn49fvmZ5CgDbsQ3j"
 const TokenFilename = "token_v2.json"
 const DefaultCacheFileName = "file_cache.json"
 
